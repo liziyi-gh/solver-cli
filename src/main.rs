20c6adabc24235e2cd21dc1111d4ba050f6e6450
liziyi <makisemengjun@gmail.com>
2024-12-09 23:41:48 +0800

    feat:

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,6 @@ fn extract_str(data: &Value, keys: &Vec<&str>) -> String {
     let mut tmp = data;
     for key in keys {
         tmp = tmp.get(key).unwrap();
-        println!("Look data: {:?}", key);
     }
 
     return String::from(tmp.as_str().unwrap());
@@ -47,6 +46,13 @@ fn extract_i32(data: &Value, keys: &Vec<&str>) -> i32 {
     }
 
     return tmp.as_i64().unwrap() as i32;
+}
+
+fn get_bet_sizes(data: &Value, key: &str) -> BetSizeOptions {
+    let bet_sizes = extract_str(data, &vec!["tree_config", key, "bet"]);
+    let raise_sizes = extract_str(data, &vec!["tree_config", key, "raise"]);
+
+    BetSizeOptions::try_from((bet_sizes.as_str(), raise_sizes.as_str())).unwrap()
 }
 
 fn main() {
@@ -74,7 +80,12 @@ fn main() {
         card_config.river = card_from_str(&river).unwrap();
         initial_state = BoardState::River;
     }
-    let bet_sizes = BetSizeOptions::try_from(("60%, e, a", "2.5x")).unwrap();
+    let oop_flop_bet_sizes = get_bet_sizes(&data, "oop_flop_bet_sizes");
+    let ip_flop_bet_sizes = get_bet_sizes(&data, "ip_flop_bet_sizes");
+    let oop_turn_bet_sizes = get_bet_sizes(&data, "oop_turn_bet_sizes");
+    let ip_turn_bet_sizes = get_bet_sizes(&data, "ip_turn_bet_sizes");
+    let oop_river_bet_sizes = get_bet_sizes(&data, "oop_river_bet_sizes");
+    let ip_river_bet_sizes = get_bet_sizes(&data, "ip_river_bet_sizes");
     let river_donk_sizes_str = extract_str(&data, &vec!["tree_config", "river_donk_sizes"]);
 
     let tree_config = TreeConfig {
@@ -83,9 +94,10 @@ fn main() {
         effective_stack: extract_i32(&data, &vec!["tree_config", "effective_stack"]),
         rake_rate: extract_float(&data, &vec!["tree_config", "rake_rate"]),
         rake_cap: extract_float(&data, &vec!["tree_config", "rake_cap"]),
-        flop_bet_sizes: [bet_sizes.clone(), bet_sizes.clone()], // [OOP, IP]
-        turn_bet_sizes: [bet_sizes.clone(), bet_sizes.clone()],
-        river_bet_sizes: [bet_sizes.clone(), bet_sizes],
+        flop_bet_sizes: [oop_flop_bet_sizes, ip_flop_bet_sizes],
+        turn_bet_sizes: [oop_turn_bet_sizes, ip_turn_bet_sizes],
+        river_bet_sizes: [oop_river_bet_sizes, ip_river_bet_sizes],
+        // TODO: donk sizes
         turn_donk_sizes: None,
         river_donk_sizes: Some(DonkSizeOptions::try_from(river_donk_sizes_str.as_str()).unwrap()),
         add_allin_threshold: 1.5,
@@ -96,8 +108,17 @@ fn main() {
     let action_tree = ActionTree::new(tree_config).unwrap();
     let mut game = PostFlopGame::with_config(card_config, action_tree).unwrap();
     game.allocate_memory(false);
-    let max_num_iterations = 1;
+    let max_num_iterations = 1000;
     let target_exploitability = game.tree_config().starting_pot as f32 * 0.005; // 0.5% of the pot
     let exploitability = solve(&mut game, max_num_iterations, target_exploitability, true);
     println!("Exploitability: {:.2}", exploitability);
+
+    // save_data_to_file(&game, "memo string", opt.output.clone(), None).unwrap();
+
+    // let (mut game2, _memo_string): (PostFlopGame, _) =
+    //     load_data_from_file(opt.output.into_os_string().into_string().unwrap(), None).unwrap();
+    // println!(
+    //     "Memory usage of the original game tree: {:.2}MB", // 11.50MB
+    //     game.target_memory_usage() as f64 / (1024.0 * 1024.0)
+    // );
 }
